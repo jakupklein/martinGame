d36aa01ac57fee4d8bc353b7c8aa361d2601f4ee
jakupklein <jakupklein@gmail.com>
2015-10-20 16:11:11 +0200

    jakup added killing zombies

--- Assets/Scripts/chooseShape.cs
+++ Assets/Scripts/chooseShape.cs
@@ -58,7 +58,7 @@ public class chooseShape : MonoBehaviour {
 		if(other.gameObject.tag == "Fist" && runCollisoinOnce) {
 			print("fist");
 			runCollisoinOnce = false;
-			counter--;
+				counter--;
 		}
 
 	}
