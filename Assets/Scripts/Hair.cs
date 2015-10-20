01599bcabf08c9281b91f12d64f4e37af7bae21f
jakupklein <jakupklein@gmail.com>
2015-10-20 12:05:08 +0200

    jakup made hair

---
+++ Assets/Scripts/Hair.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hair : MonoBehaviour {
+
+//public GameObject test;
+	//void Start() {
+	public void Start(){
+	MeshFilter meshFilter = GetComponent<MeshFilter>();
+
+	if (meshFilter==null){
+	    Debug.LogError("MeshFilter not found!");
+	    return;
+
+
+        }
+
+    Vector3 p0 = new Vector3(-0.5f,-Mathf.Sqrt(0.75f)/2f,-Mathf.Sqrt(0.75f)/2f);
+
+	Vector3 p1 = p0 + new Vector3(1,0,0);
+	Vector3 p2 = p0 + new Vector3(0.5f,0,Mathf.Sqrt(0.75f));
+	Vector3 p3 = p0 + new Vector3(0.5f,Mathf.Sqrt(0.75f),Mathf.Sqrt(0.75f)/3);
+
+	Vector3 fixCenterOffset = new Vector3(0,-0.223f,-0.144f);
+	//Vector3 fixCenterOffset = new Vector3(
+	//									(p0.x + p1.x + p2.x + p3.x)/3,
+	//									(p0.y + p1.y + p2.y + p3.y)/3,
+	//									(p0.z + p1.z + p2.z + p3.z)/3
+	//									);
+//	test.transform.position = fixCenterOffset;
+	Vector3[] normals = {p0 + fixCenterOffset,p1 + fixCenterOffset,p2 + fixCenterOffset,p3 + fixCenterOffset};
+	for(int i = 0; i < normals.Length; i++){
+		normals[i] = normals[i].normalized;
+	}
+	Mesh mesh = meshFilter.mesh;
+	if (mesh == null){
+	    meshFilter.mesh = new Mesh();
+	    mesh = meshFilter.mesh;
+	}
+	mesh.Clear();
+	mesh.vertices = new Vector3[]{p0,p1,p2,p3};
+	mesh.triangles = new int[]{
+	    0,1,2,
+	    0,2,3,
+	    2,1,3,
+	    0,3,1
+	};
+	mesh.normals = normals;
+	//mesh.RecalculateNormals();
+	mesh.RecalculateBounds();
+	mesh.Optimize();
+	//mesh.Optimize();
+	//	var tem = new QuadTest();
+	}
+
+
+
+
+}
--- Assets/Scripts/InstantiateZombies.cs
+++ Assets/Scripts/InstantiateZombies.cs
@@ -4,16 +4,19 @@ using System.Collections;
 public class InstantiateZombies : MonoBehaviour {
 
 	public int numberOfZombiesOnStart;
-	public GameObject Zombie;
+	GameObject Zombie;
 	// Use this for initialization
 	void Start () {
+		Zombie = Instantiate(Resources.Load("Zombie")) as GameObject;
+
 		if(numberOfZombiesOnStart == null){
 			print("Number of zombies not set, default value is 5.");
 			numberOfZombiesOnStart = 5;
 		}
 		for(int i = 0; i < numberOfZombiesOnStart; i ++){
-			//Instantiate(Zombie);
-			//Zombie = Instantiate(Resources.Load("Zombie")) as GameObject;
+
+			Zombie.transform.position = new Vector3(Random.Range(-5f, 5),3f,Random.Range(-5f, 5));
+			Instantiate(Zombie);
 		}
 
 	}
