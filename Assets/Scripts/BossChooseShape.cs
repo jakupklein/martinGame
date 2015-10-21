358c6c651746de472d410227364a3a238503edb3
jakupklein <jakupklein@gmail.com>
2015-10-21 11:00:28 +0200

    jakup fixed boss problem

--- Assets/Scripts/BossChooseShape.cs
+++ Assets/Scripts/BossChooseShape.cs
@@ -26,25 +26,30 @@ public class BossChooseShape : MonoBehaviour {
 	}
 
 	public void ChangeShape(int shapeNumber, Color shapeColor) {
+
 		switch(shapeNumber)
 		{
 			case 0:
+				//print("0");
 				tetrahedron.MakeMesh();
 				transform.localScale = new Vector3(1.63965f, 1.63965f, 1.63965f);
 				transform.localPosition = new Vector3(0f, 0.137f, 0.071f);
 				rend.material.color = shapeColor;
 				break;
 			case 1:
+				//print("1");
 				octahedron.MakeMesh();
 				transform.localScale= new Vector3(2.165706f, 2.165706f, 2.165706f);
 				rend.material.color = shapeColor;
 				break;
 			case 2:
+				//print("2");
 				dodecahedron.MakeMesh();
 				transform.localScale = new Vector3(1.04493f, 1.04493f, 1.04493f);
 				rend.material.color = shapeColor;
 				break;
 			case 3:
+				//print("3");
 				icosahedron.MakeMesh();
 				transform.localScale = new Vector3(1,1,1);
 				rend.material.color = shapeColor;
