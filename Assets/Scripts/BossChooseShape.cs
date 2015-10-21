b78dbd61023c6e4f0a38c0322f446d263d7d52a7
jakupklein <jakupklein@gmail.com>
2015-10-21 09:42:49 +0200

    Jakup made the boss better
    I also changed the zombie colors

---
+++ Assets/Scripts/BossChooseShape.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossChooseShape : MonoBehaviour {
+
+	Tetrahedron tetrahedron;	//4
+	Octahedron octahedron;		//8
+	Dodecahedron dodecahedron;	//12
+	Icosahedron icosahedron;	//20
+	private float rotationSpeed;
+	private Renderer rend;
+
+	void Start () {
+		rotationSpeed = Random.Range(-5f, 5f);
+		rend = gameObject.GetComponent<Renderer>();
+
+        tetrahedron  = gameObject.GetComponent<Tetrahedron>();
+		octahedron   = gameObject.GetComponent<Octahedron>();
+		dodecahedron = gameObject.GetComponent<Dodecahedron>();
+		icosahedron  = gameObject.GetComponent<Icosahedron>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.up * rotationSpeed);
+	}
+
+	public void ChangeShape(int shapeNumber, Color shapeColor) {
+		switch(shapeNumber)
+		{
+			case 0:
+				tetrahedron.MakeMesh();
+				transform.localScale = new Vector3(1.63965f, 1.63965f, 1.63965f);
+				transform.localPosition = new Vector3(0f, 0.137f, 0.071f);
+				rend.material.color = shapeColor;
+				break;
+			case 1:
+				octahedron.MakeMesh();
+				transform.localScale= new Vector3(2.165706f, 2.165706f, 2.165706f);
+				rend.material.color = shapeColor;
+				break;
+			case 2:
+				dodecahedron.MakeMesh();
+				transform.localScale = new Vector3(1.04493f, 1.04493f, 1.04493f);
+				rend.material.color = shapeColor;
+				break;
+			case 3:
+				icosahedron.MakeMesh();
+				transform.localScale = new Vector3(1,1,1);
+				rend.material.color = shapeColor;
+				break;
+
+		}
+	}
+}
---
+++ Assets/Scripts/CreateBoss.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateBoss : MonoBehaviour {
+
+	BossChooseShape childScript;
+
+    Tetrahedron tetrahedron;	//4
+	Octahedron octahedron;		//8
+	Dodecahedron dodecahedron;	//12
+	Icosahedron icosahedron;	//20
+
+	private int endurance = 3;
+
+	public int counter;
+	private int damage;
+	private bool runCollisoinOnce = true;
+
+	public Color tetrahedronColor;
+	public Color octahedronColor;
+	public Color dodecahedronColor;
+	public Color icosahedronColor;
+
+	private Color[] colorArray;
+
+	// Use this for initialization
+	void Start () {
+		colorArray = new Color[]{ tetrahedronColor,
+								  octahedronColor,
+								  dodecahedronColor,
+								  icosahedronColor,
+								  icosahedronColor};
+
+		for(int i = 0; i < transform.childCount; i++) {
+			childScript = transform.GetChild(i).gameObject.GetComponent<BossChooseShape>();
+			childScript.ChangeShape(i, colorArray[i]);
+		}
+	}
+
+	void Update(){
+		runCollisoinOnce = true;
+
+		if(damage >= endurance * 4){
+			//if(transform.childCount == 0 + 1)
+			transform.GetChild(0).gameObject.SetActive(false);
+		}else if(damage >= endurance * 3){
+			//if(transform.childCount == 1 + 1)
+			transform.GetChild(1).gameObject.SetActive(false);
+		}else if(damage >= endurance * 2){
+			//if(transform.childCount == 2 + 1)
+			transform.GetChild(2).gameObject.SetActive(false);
+		}else if(damage >= endurance * 1){
+			//if(transform.childCount == 3 + 1)
+			transform.GetChild(3).gameObject.SetActive(false);
+		}
+
+	}
+
+	public void BossRebirth() {
+		transform.GetChild(0).gameObject.SetActive(true);
+		transform.GetChild(1).gameObject.SetActive(true);
+		transform.GetChild(2).gameObject.SetActive(true);
+		transform.GetChild(3).gameObject.SetActive(true);
+		damage = 0;
+	}
+
+	void OnCollisionEnter(Collision other) {
+
+		if(other.gameObject.tag == "Fist" && runCollisoinOnce) {
+			runCollisoinOnce = false;
+				damage++;
+
+		}
+
+	}
+}
